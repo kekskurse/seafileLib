f5b60e1af88592e6ee76840ff1e151d88021c39a
kruemel <kruemel@schredder.pw>
2014-05-05 14:00:01 +0200

    Test

---
+++ sspssp/seafilelib/Seafile.php
@@ -0,0 +1,27 @@
+<?php
+namespace sspssp/seafilelib;
+class Seafile
+{
+	private $server = NULL;
+	private $user = NULL;
+	private $pw = NULL;
+	public function __construct()
+	{
+		$this->curl = new anlutro\cURL\cURL;
+	}
+	public function setServer($server)
+	{
+		$this->server = $server;
+	}
+	public function setUser($user, $pw)
+	{
+		$this->user = $user;
+		$this->pw = $pw;
+	}
+	public function ping()
+	{
+		$r = $this->curl->get('http://seafile.byte.gs/api2/ping/');
+		var_dump($r);
+	}
+}
+?>
