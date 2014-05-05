973db0c21754a28f1bda2e6217ab30829127b1b6
kruemel <kruemel@schredder.pw>
2014-05-05 15:11:54 +0200

    Messages

---
+++ sspssp/seafilelib/Messages.php
@@ -0,0 +1,28 @@
+<?php
+namespace sspssp\seafilelib;
+class Messages()
+{
+	public function __construct($seafile)
+	{
+		$this->sf = $seafile;
+	}
+	public function listMessages($mail)
+	{
+		$r = $this->sf->sendRequest("get", "/api2/user/msgs/".urlencode($mail)."/");
+		$r = json_decode($r, true);
+		return $r;
+	}
+	public function replay($mail, $message)
+	{
+		$r = $r = $this->sf->sendRequest("post", "/api2/user/msgs/".urlencode($mail)."/", ["message"]=>$message);
+		$r = json_decode($r, true);
+		return $r;
+	}
+	public function countUnseenMessages()
+	{
+		$r = $this->sf->sendRequest("get", "/api2/unseen_messages/");
+		$r = json_decode($r, true);
+		return $r;
+	}
+}
+?>
